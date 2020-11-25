ce97dee8adc23ff67f5476863ab3b1ed2f5ea715
iamtbc <kosuke.tabuchi@mirumill.com>
2020-11-26 03:39:49 +0900

    chore: upgrade storybook 6.1

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
-  stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: [
+    '../../docs/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-links']
 }
