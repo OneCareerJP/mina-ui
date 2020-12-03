57594dbc50387d2b1885211e1a1292ca4ac4258f
iamtbc <kosuke.tabuchi@mirumill.com>
2020-12-04 02:53:47 +0900

    feat: add panel component

---
+++ src/components/panel/index.stories.js
@@ -0,0 +1,25 @@
+import MPanel from "./main.vue";
+
+export default {
+  title: "Panel",
+  component: MPanel,
+  argTypes: {
+    rounded: {
+      control: { type: "select", options: [true, false] }
+    }
+  }
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { MPanel },
+  template: `
+    <MPanel v-bind="$props">
+      <div style="padding:100px; text-align:center;">panel content</div>
+    </MPanel>`
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  rounded: false
+};
